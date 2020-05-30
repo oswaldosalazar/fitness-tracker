2ddfa64619fc0089bf4ad4a407dabf0c03777e2c
oswaldosalazar <oswaldo.r.salazar@gmail.com>
2020-05-30 17:23:08 -0600

    Started training logic

--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -5,6 +5,9 @@ import {
   Output
 } from '@angular/core';
 
+import { TrainingService } from './../training.service';
+import { Exercise } from './../exercise.model';
+
 @Component({
   selector: 'app-new-training',
   templateUrl: './new-training.component.html',
@@ -12,10 +15,13 @@ import {
 })
 export class NewTrainingComponent implements OnInit {
   @Output() trainingStart = new EventEmitter<void>();
+  exercises: Exercise[] = [];
 
-  constructor() {}
+  constructor(private trainingService: TrainingService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.exercises = this.trainingService.getAvailableExercises();
+  }
 
   onStartTraining() {
     this.trainingStart.emit();
