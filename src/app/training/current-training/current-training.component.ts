243806750e0a2bf70299b8bcdc84f05cf4b5424f
oswaldosalazar <oswaldo.r.salazar@gmail.com>
2020-05-31 19:40:58 -0600

    Finished data table

--- src/app/training/current-training/current-training.component.ts
+++ src/app/training/current-training/current-training.component.ts
@@ -1,11 +1,8 @@
-import {
-  Component,
-  OnInit,
-  Output,
-  EventEmitter
-} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+
 import { StopTrainingComponent } from './stop-training.component';
+import { TrainingService } from './../training.service';
 
 @Component({
   selector: 'app-current-training',
@@ -13,23 +10,29 @@ import { StopTrainingComponent } from './stop-training.component';
   styleUrls: ['./current-training.component.css']
 })
 export class CurrentTrainingComponent implements OnInit {
-  @Output() trainingExit = new EventEmitter();
   progress = 0;
   timer;
 
-  constructor(private dialog: MatDialog) {}
+  constructor(
+    private dialog: MatDialog,
+    private trainingService: TrainingService
+  ) {}
 
   ngOnInit(): void {
     this.startOrResumeTimer();
   }
 
   startOrResumeTimer() {
+    const step =
+      (this.trainingService.getRunningExercise().duration / 100) *
+      1000;
     this.timer = setInterval(() => {
-      this.progress += 20;
+      this.progress += 1;
       if (this.progress >= 100) {
+        this.trainingService.completeExercise();
         clearInterval(this.timer);
       }
-    }, 1000);
+    }, step);
   }
 
   onStop() {
@@ -41,7 +44,9 @@ export class CurrentTrainingComponent implements OnInit {
     });
 
     dialogRef.afterClosed().subscribe(result => {
-      result ? this.trainingExit.emit() : this.startOrResumeTimer();
+      result
+        ? this.trainingService.cancelExercise(this.progress)
+        : this.startOrResumeTimer();
     });
   }
 }
--- src/app/training/past-training/past-training.component.ts
+++ src/app/training/past-training/past-training.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  AfterViewInit
+} from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+
+import { Exercise } from '../exercise.model';
+import { TrainingService } from './../training.service';
+import { MatSort } from '@angular/material/sort';
+import { MatPaginator } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-past-training',
   templateUrl: './past-training.component.html',
   styleUrls: ['./past-training.component.css']
 })
-export class PastTrainingComponent implements OnInit {
+export class PastTrainingComponent implements OnInit, AfterViewInit {
+  displayedColumns = [
+    'date',
+    'name',
+    'duration',
+    'calories',
+    'state'
+  ];
+  dataSource = new MatTableDataSource<Exercise>();
 
-  constructor() { }
+  @ViewChild(MatSort, { static: false }) sort: MatSort;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  constructor(private trainingService: TrainingService) {}
 
   ngOnInit(): void {
+    this.dataSource.data = this.trainingService.getCompletedOrCancelledExercises();
   }
 
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+  }
+
+  doFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
 }
