a9ec76c0d0bd5675b5f5a599f2f90b7789443e6b
oswaldosalazar <oswaldo.r.salazar@gmail.com>
2020-06-05 14:56:27 -0600

    Added env var to production file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyC8pFWCpYz3shGMCH44XcN4qgh1dQ4jNqU',
+    authDomain: 'ng-fitness-os.firebaseapp.com',
+    databaseURL: 'https://ng-fitness-os.firebaseio.com',
+    projectId: 'ng-fitness-os',
+    storageBucket: 'ng-fitness-os.appspot.com',
+    messagingSenderId: '220412753809',
+    appId: '1:220412753809:web:d6c3cec69ab99672d74c30',
+    measurementId: 'G-XGJV3R6FVW'
+  }
 };
